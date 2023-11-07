92e858e16844359d5556c836654eb512373bcc9a
danielaser <daniela.serrano.dev@gmail.com>
2023-11-06 20:14:24 -0500

    first commit. Tic Tac Toe game made in React

---
+++ Readme.md
@@ -0,0 +1,43 @@
+# React Tic-Tac-Toe - Daniela Serrano
+
+This is a tipical Tic Tac Toe game made in React. I built it as a result of the React course by Maximilian Schwarzmüller (Udemy).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The exercise](#the-exercise)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The exercise
+
+Users should be able to:
+
+- Enter and edit your Player name.
+- Play Tic Tac Toe with another player.
+
+### Screenshot
+
+![](./src/assets/Screenshot.png)
+
+### Links
+
+- Live Site URL: [Tic-Tac-Toe React](https://card-counter-score.netlify.app/)
+
+## My process
+
+### Built with
+
+- React
+- CSS custom properties
+- JSX files
+
+## Author
+
+- Website - [Daniela Serrano](https://danielaser.github.io/)
+- Github - [@danielaser](https://github.com/danielaser)
---
+++ src/components/Player.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleButton() {
+    setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+    //target emit the event. value because is the value that user entered
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleButton}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
